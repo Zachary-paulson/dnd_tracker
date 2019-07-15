ad4f72c5e92e84dcd6722bd531b6f37ed363d3b4
Zachary Paulson <zachary.paulson@outlook.com>
2019-07-15 12:30:13 -0700

    initial commit

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+//middleware for proxy server
+//Doesn't need to be imported anywhere
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/api', { target: 'http://localhost:5000/' }));
+};
